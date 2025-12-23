5ee8e9d940c1d4db9eabc55bee7b61d37a20b77f
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-12-23 15:42:11 +0100

    Add the connection to database

---
+++ config/database.php
@@ -0,0 +1,33 @@
+<?php
+
+    class Database {
+        private $host = 'localhost';    
+        private $db = 'CoachPro';
+        private $user = 'root';
+        private $password = 'Youcode@2025';
+
+        private ?PDO $conn = null;
+        
+
+        public function getConnection() {
+            $this->conn = null;
+            
+            if($this->conn === null) {
+                try {
+                    $this->conn = new PDO(
+                        "mysql:host={$this->host};dbname={$this->db}", $this->user, $this->password
+                    );
+
+                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    
+                }catch (PDOException $err) {
+                    die('Database connection problem: '. $err->getMessage());   
+                }
+            }
+            return $this->conn;
+        }
+
+    }
+
+
+?>
