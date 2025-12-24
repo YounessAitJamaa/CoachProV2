fe14b1761c280a2562efb6f1c317273839370eec
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-12-24 20:16:12 +0100

    first commit

---
+++ workshop/Challenge1.sql
@@ -0,0 +1,42 @@
+/* =====================================================
+   nombre total de séances créées
+===================================================== */
+
+SELECT u.nom, u.prenom, COUNT(s.id) AS total
+FROM seances s
+JOIN coachs c ON s.coach_id = c.user_id
+JOIN users u ON c.user_id = u.id
+GROUP BY u.nom, u.prenom
+
+/* =====================================================
+   nombre de séances réservées
+===================================================== */
+
+SELECT u.nom, u.prenom, COUNT(s.id) AS total
+FROM seances s
+JOIN coachs c ON s.coach_id = c.user_id
+JOIN users u ON c.user_id = u.id
+WHERE s.statut = 'reservee'
+GROUP BY u.nom, u.prenom
+
+/* =====================================================
+   taux de réservation (%)
+===================================================== */
+
+    SELECT u.nom, u.prenom, (COUNT(s.id) / sum(s.id) * 100) AS total
+    FROM seances s
+    JOIN coachs c ON s.coach_id = c.user_id
+    JOIN users u ON c.user_id = u.id
+    GROUP BY u.nom, u.prenom
+
+
+/* =====================================================
+   seulement les coachs ayant ≥3 séances
+===================================================== */
+
+SELECT u.nom, u.prenom, COUNT(s.id) AS total
+FROM seances s
+JOIN coachs c ON s.coach_id = c.user_id
+JOIN users u ON c.user_id = u.id
+GROUP BY u.nom, u.prenom
+HAVING COUNT(*) >= 3
